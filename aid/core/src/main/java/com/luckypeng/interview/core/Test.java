73a8fa4da8dc8789c0621e2be647bd9e17ac6a28
coalchan <coalchan@gmail.com>
2019-05-30 18:10:15 +0800

    add: 工具搭建

---
+++ aid/core/src/main/java/com/luckypeng/interview/core/Test.java
@@ -0,0 +1,10 @@
+package com.luckypeng.interview.core;
+
+/**
+ * @author coalchan
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("interview-core");
+    }
+}
